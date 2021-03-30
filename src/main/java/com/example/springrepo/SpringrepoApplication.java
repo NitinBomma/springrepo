0d3b9ba1a4ac9d6d6ae844ff670a70b47e670e67
unknown <you@example.com>
2021-03-30 18:26:44 +0530

    i have done changes

---
+++ src/main/java/com/example/springrepo/SpringrepoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrepoApplication.class, args);
+	}
+//this is nitin
+
+}
